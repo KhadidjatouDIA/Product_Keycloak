475ae998477b4e58dfeef09919073efcd5d50b01
3011alhamdoulilah <sokhnakhadidjah@gmail.com>
2025-01-15 21:24:27 +0000

    update

--- web_keycloack/src/main/java/com/dev/web_keycloack/web_keycloack/Controller/ProductController.java
+++ web_keycloack/src/main/java/com/dev/web_keycloack/web_keycloack/Controller/ProductController.java
@@ -1,13 +1,12 @@
 package com.dev.web_keycloack.web_keycloack.Controller;
 
+import com.dev.web_keycloack.web_keycloack.Entities.Product;
 import com.dev.web_keycloack.web_keycloack.Services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @RequestMapping("/products")
 @RequiredArgsConstructor
@@ -16,15 +15,18 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public void addProduct(ModelMap modelMap, @ModelAttribute String name){
+    public String addProduct(@RequestParam String name) {
         productService.addProduct(name);
+        return "redirect:/products"; // Redirige après ajout
     }
 
     @GetMapping
-    public String getList(ModelMap modelMap ){
-        var list= productService.getList();
-        modelMap.addAttribute("listProducts",list);
+    public String getList(ModelMap modelMap) {
+        var list = productService.getList();
+        modelMap.addAttribute("products", list);
+        modelMap.addAttribute("product", new Product()); // Initialisation
         return "products";
     }
-
 }
+
+
